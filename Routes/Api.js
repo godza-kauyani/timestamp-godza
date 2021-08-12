f1ad046b6b85d801acb3fe61cc54652eb9ca9ed3
godzakauyani <kauyanigodfrey@gmail.com>
2021-08-12 10:56:49 +0200

    added comments

--- Routes/Api.js
+++ Routes/Api.js
@@ -1,27 +1,27 @@
-/*a request to /api/timestamp/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }*/
-/*
-Your project can handle dates that can be successfully parsed by new Date(date_string)
-*/
-/*
-\If the input date string is invalid, the api returns an object having the structure { error : "Invalid Date" }
-*/
-/*
-An empty date parameter should return the current time in a JSON object with a unix key
 
-An empty date parameter should return the current time in a JSON object with a utc key
-*/
 const {Router} = require('express');
 const router = Router();
 const moment = require('moment');
+/*
+An empty date parameter should return the current time in a JSON object with a unix key
+An empty date parameter should return the current time in a JSON object with a utc key
+*/
 
-router.get('/timestamp/',(req,res)=>{
+router.get('/',(req,res)=>{
   const result = {
     unix:new Date().getTime(),
     utc:new Date().toUTCString()
   }
   res.json(result)
 })
-router.get('/timestamp/:date',(req,res)=>{
+
+/*
+A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds
+
+A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+*/
+
+router.get('/:date/',(req,res)=>{
   date_string = req.params.date
   const timestamp = date_string.includes('-')?date_string:+date_string
   if(moment(timestamp).isValid()){
@@ -30,7 +30,11 @@ router.get('/timestamp/:date',(req,res)=>{
       utc:new Date(timestamp).toUTCString()
     })
   }
+//If the input date string is invalid, the api returns an object having the structure { error : "Invalid Date" }
+
   res.json({error:'Invalid Date'})
     
 });
+
+
 module.exports= router;
